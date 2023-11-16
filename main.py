ad0b19180b9ae45c45171ff09c91ff01233f873f
Houda222 <houda.ghallab@imt-atlantique.net>
2023-11-16 16:00:55 +0100

    modified main

--- main.py
+++ main.py
@@ -1,87 +1,13 @@
-#%%
+
 import math, os
 import cv2
 import numpy as np
 from sklearn.cluster import KMeans, DBSCAN
 from ultralytics import YOLO
 from mySgf import GoBoard, GoSgf
-
-def imshow_(image):
-    screen_width, screen_height = 1920, 1080  # Replace with your screen resolution or use a library to detect it dynamically
-
-    # Calculate the scaling factors for width and height
-    width_scale = screen_width / float(image.shape[1])
-    height_scale = screen_height / float(image.shape[0])
-
-    # Choose the smaller scaling factor to fit the image within the screen dimensions
-    scale = min(width_scale, height_scale)
-
-    # Resize the image with the calculated scale
-    resized_image = cv2.resize(image, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
-
-    # Get the dimensions of the resized image
-    window_width, window_height = resized_image.shape[1], resized_image.shape[0]
-
-    # Create a window with the determined size
-    cv2.namedWindow('img', cv2.WINDOW_KEEPRATIO)
-    # cv2.resizeWindow('img', window_width, window_height)
-    cv2.imshow('img', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def findContours_(image, imageToDrawOn=None):
-    # ret, thresh = cv2.threshold(image, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    if not imageToDrawOn is None:
-        cv2.drawContours(imageToDrawOn, contours, -1, (0, 255, 0), 1)
-    return contours, hierarchy
-
-def HoughLinesP_(image, imageToDrawOn):
-    lines = cv2.HoughLinesP(image, rho=1, theta=np.pi / 180, threshold=100, minLineLength=20, maxLineGap=100)
-    img = np.zeros_like(imageToDrawOn)
-    
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
-                
-    return lines, img
-
-def Canny_(image):
-    high_thresh, _ = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    lowThresh = 0.5*high_thresh
-    high_thresh = 150
-    lowThresh = 50
-    return cv2.Canny(image, lowThresh, high_thresh)
-
-def cartesian_to_polar(x1, y1, x2, y2):
-    # Calculate the angle theta in radians
-    if x2 - x1 != 0:
-        theta = math.atan((y2 - y1) / (x2 - x1))
-    else:
-        theta = math.pi / 2  # Vertical line, atan(inf) = pi/2
-    
-    # Calculate the radius r
-    r = x1 * np.cos(theta) + y1 * np.sin(theta)
-    
-    return r, theta
-
-def draw_lines(lines, img=None, color=(0, 0, 255), thickness=1):
-    global image
-    if img is None:
-        img = np.zeros_like(image)
-    for line in lines:
-        x1, y1, x2, y2 = line
-        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
-
-def draw_points(points, img=None, color=(0, 0, 255), thickness=3):
-    global image
-    if img is None:
-        img = np.zeros_like(image)
-    for point in points:
-        cv2.circle(img, point, 3, color, thickness)
-    imshow_(img)
+import threading
+import copy
+import traceback
 
 def interpolate(x1, y1, x2, y2, image_width=600, image_height=600):
     "y = slope * x + b"
@@ -128,24 +54,6 @@ def adress_lines(lines):
 def are_similar(line1, line2, threshold=10):
     return np.all(np.abs(line1 - line2) <= threshold)
 
-# def removeDuplicates(lines):
-#     grouped_lines = {}
-#     for line in lines:
-#         x1, y1, x2, y2 = line
-#         found = False
-#         for key in grouped_lines.keys():
-#             if are_similar(key, line):
-#                 grouped_lines[key] = grouped_lines[key] + [line]
-#                 found = True
-#                 break
-#         if not found:
-#             grouped_lines[(x1, y1, x2, y2)] = [line]
-
-#     final_lines = []
-#     for key in grouped_lines.keys():
-#         final_lines.append(np.mean(grouped_lines[key], axis=0))
-    
-#     return np.array(final_lines).astype(np.int32)
 
 def removeDuplicates(lines):
     grouped_lines = {}
@@ -185,11 +93,6 @@ def removeDuplicates(lines):
         mean_line = np.mean(second_dict[key], axis=0).astype(dtype=int)
         final_lines.append(mean_line)
     
-    
-    # print(np.sort(np.array(list(second_dict.keys())), axis=0))
-    # print(np.sort(np.array(list(grouped_lines.keys())), axis=0))
-    
-    
     return np.array(final_lines).astype(np.int32)
 
 
@@ -218,18 +121,6 @@ def get_angle(x1, y1, x2, y2):
         angle = math.pi / 2
     return angle
 
-#%%
-
-def get_blurred_img(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.bilateralFilter(gray,5,100,100)
-    
-    # dst = cv2.Laplacian(gray, cv2.CV_16S, ksize=3)
-    # gray = cv2.convertScaleAbs(dst)
-
-    # blur = cv2.GaussianBlur(gray,(5,5), 3)
-    # gray = cv2.subtract(gray,blur)
-    return image, gray
 
 def clean_lines(lines, image_width, image_height):
     global image
@@ -269,13 +160,10 @@ def cluster_orientation(lines):
 
 def create_board(intersections, board_size=(19,19)):
     cleaned_intersections = intersections.tolist()
-    # for inter in intersections:
-    #     if inter[0] > 2 and inter[0] < 598 and inter[1] < 598 and inter[1] > 2:
-    #         cleaned_intersections.append(inter)
+ 
     
     
     cleaned_intersections.sort(key=lambda x: (x[1], x[0]))
-    print("nbre of intersections", len(cleaned_intersections))
     
     board = {}
     for j in range(0, 19):
@@ -293,7 +181,6 @@ def detect_intersections(cluster_1, cluster_2, image):
     
     for v_line in cluster_1:
         for h_line in cluster_2:
-            print(adress_lines([v_line]), adress_lines([h_line]))
             inter = intersect(v_line, h_line)
             
             if all(image.shape[:1] > inter) and all(inter >= 0):
@@ -301,60 +188,8 @@ def detect_intersections(cluster_1, cluster_2, image):
     
     return np.array(intersections)
 
-def draw_corners(intersections, img):
-    intersections.sort(key= lambda x: x[0] + x[1])
-    tl = intersections[0]
-    br = intersections[-1]
-    cv2.circle(img,tl,3,255,10)
-    cv2.circle(img,br,3,255,10)
 
-    intersections.sort(key= lambda x: image.shape[0] - x[0] + x[1])
-    tr = intersections[0]
-    bl = intersections[-1]
-    cv2.circle(img,tr,3,255,10)
-    cv2.circle(img,bl,3,255,10)
     
-    corners = np.array([tl, tr, br, bl])
-
-    cv2.polylines(img, [np.array([[tl, tr, br, bl]])], isClosed=True, color=(255,0,0), thickness=2)
-    imshow_(img)
-    return corners
-
-def get_corner_from_box(intersections, box, location): # box = (x1, y1, x2, y2)
-    candidates = []
-    for inter in intersections:
-        if box[0] <= inter[0] <= box[2] and box[1] <= inter[1] <= box[3]:
-            candidates.append(inter)
-    
-    if len(candidates) == 0:
-        raise Exception(f"no intersections ware found at {location} corner")
-    if len(candidates) == 1:
-        return candidates[0]
-    
-    if location == "NO":
-        return min(candidates, key=lambda x: x[0] + x[1])
-    if location == "SE":
-        return max(candidates, key=lambda x: x[0] + x[1])
-    if location == "NE":
-        return min(candidates, key=lambda x: box[2] - x[0] + x[1])
-    if location == "SO":
-        return max(candidates, key=lambda x: box[2] - x[0] + x[1])
-    
-    raise Exception("Nothing was found")
-
-def get_corners(intersections, boxes):
-    box_no = min(boxes, key=lambda x: x[0] + x[1] + x[2] + x[3])
-    box_se = max(boxes, key=lambda x: x[0] + x[1] + x[2] + x[3])
-    box_ne = max(boxes, key=lambda x: x[0] - x[1] + x[2] - x[3])
-    box_so = min(boxes, key=lambda x: x[0] - x[1] + x[2] - x[3])
-    
-    corner_no = get_corner_from_box(intersections, box_no, "NO")
-    corner_se = get_corner_from_box(intersections, box_se, "SE")
-    corner_ne = get_corner_from_box(intersections, box_ne, "NE")
-    corner_so = get_corner_from_box(intersections, box_so, "SO")
-    
-    return np.array([corner_no, corner_ne, corner_se, corner_so])
-
 def define_moves(white_stones_transf, black_stones_transf, transformed_intersections):
     global transformed_image
     
@@ -366,7 +201,6 @@ def define_moves(white_stones_transf, black_stones_transf, transformed_intersect
 
     for stone in white_stones_transf:
         
-        # cv2.circle(transformed_image, np.array(stone).astype(dtype=np.int32), 3, (0, 0, 255), 2)
         
         nearest_corner = None
         closest_distance = 100000
@@ -376,11 +210,9 @@ def define_moves(white_stones_transf, black_stones_transf, transformed_intersect
                 nearest_corner = tuple(inter)
                 closest_distance = distance
         moves.append(("W", (Board[nearest_corner][0], 18 - Board[nearest_corner][1])))
-        # cv2.line(transformed_image, (int(stone[0]), int(stone[1])), nearest_corner, (0, 255, 255), 2)
             
     for stone in black_stones_transf:
         
-        # cv2.circle(transformed_image, np.array(stone).astype(dtype=np.int32), 3, (0, 0, 255), 2)
         
         nearest_corner = None
         closest_distance = 100000
@@ -390,15 +222,11 @@ def define_moves(white_stones_transf, black_stones_transf, transformed_intersect
                 nearest_corner = tuple(inter)
                 closest_distance = distance
         moves.append(("B", (Board[nearest_corner][0], 18 - Board[nearest_corner][1])))
-        # cv2.line(transformed_image, (int(stone[0]), int(stone[1])), nearest_corner, (0, 255, 255), 2)
         
     return moves
 
-# %%
-##############################""
 def calculate_distances(lines):
     distances = [(np.linalg.norm(lines[i + 1][:2]-lines[i][:2]) + np.linalg.norm(lines[i + 1][2:]-lines[i][2:])) / 2 for i in range(len(lines) - 1)]
-    # distances = [np.linalg.norm(lines[i + 1][:2] - lines[i][:2]) for i in range(len(lines) - 1)]
     return distances
 
 def find_common_distance(distances, target_distance=30):
@@ -414,7 +242,6 @@ def find_common_distance(distances, target_distance=30):
     for label in unique_labels:
         means = np.append(means, np.mean(distances_[labels==label]))
         label_index = np.append(label_index, label)
-        # plt.scatter(np.ones(len(distances_[labels==label]))*10, distances_[labels==label])
 
     index = np.argmin(np.abs(means - target_distance))
     
@@ -507,23 +334,7 @@ def non_max_suppression(boxes, overlapThresh=0.5):
 	# integer data type
 	return boxes[pick].astype("int")
 
-#%%
-# def get_four_corners(corner_centers):
-#         eps = 30
-#         result_corners = []
 
-#         for i in range(len(corner_centers)):
-#             valid_corner = True
-
-#             for j in range(len(result_corners)):
-#                 if np.linalg.norm(corner_centers[i] - result_corners[j]) < eps:
-#                     valid_corner = False
-#                     break
-
-#             if valid_corner:
-#                 result_corners.append(corner_centers[i])
-
-#         return result_corners
 
 def model_processing(model_results, perspective_matrix, frame):
     global cluster_vertical, cluster_horizontal, all_intersections, empty_intersections, empty_corner, empty_edge
@@ -561,8 +372,6 @@ def model_processing(model_results, perspective_matrix, frame):
     cluster_labels = kmeans.labels_
     unique_labels = np.unique(cluster_labels)
     
-    colors = [(240, 42, 189), (77, 110, 250), (151, 23, 16), (123, 87, 66), (205, 51, 191), (94, 215, 51), (10, 100, 203), (16, 223, 22), (130, 110, 245), (173, 85, 89), (212, 146, 129), (89, 42, 8), (208, 191, 72), (51, 197, 91), (111, 30, 187), (191, 192, 198), (187, 55, 123), (1, 224, 111), (67, 122, 173)]
-
     cluster_vertical = []
     i = 0
     for label in unique_labels:
@@ -590,15 +399,10 @@ def model_processing(model_results, perspective_matrix, frame):
     cluster_labels = kmeans.labels_
     unique_labels = np.unique(cluster_labels)
     
-    colors = [(240, 42, 189), (77, 110, 250), (151, 23, 16), (123, 87, 66), (205, 51, 191), (94, 215, 51), (10, 100, 203), (16, 223, 22), (130, 110, 245), (173, 85, 89), (212, 146, 129), (89, 42, 8), (208, 191, 72), (51, 197, 91), (111, 30, 187), (191, 192, 198), (187, 55, 123), (1, 224, 111), (67, 122, 173)]
 
     cluster_horizontal = []
     i = 0
     for label in unique_labels:
-        # print(all_intersections_x[cluster_labels==label])
-        # plt.scatter(np.arange(len(all_intersections_x[cluster_labels==label])), all_intersections_x[cluster_labels==label])
-        # draw_points(all_intersections[cluster_labels==label].astype(dtype=int), frame, colors[i])
-        # i += 1
         line = all_intersections[cluster_labels==label]
         line = line[np.argsort(line[:, 0])]
         first_endpoint = line[0]
@@ -634,12 +438,7 @@ def master(frame):
     # corners = corner_centers.tolist()
     corners = corner_centers
     
-    # corners.sort(key=lambda x: x[1])
-    # upper = corners[:2]
-    # lower = corners[2:]
-    # upper.sort(key=lambda x: x[0])
-    # lower.sort(key=lambda x: x[0], reverse=True)
-    # corners = upper + lower
+ 
     corners = corners[corners[:, 1].argsort()]
     
     upper = corners[:2]
@@ -651,13 +450,6 @@ def master(frame):
     corners = np.concatenate((upper, lower))
 
     input_points = np.array(corners, dtype=np.float32)
-    # print("input to perspective", input_points.shape)
-    # input_points_copy = input_points.copy().astype(dtype=int)
-    
-    # frame_copy = frame.copy()
-    # for point in input_points_copy:
-    #     cv2.circle(frame_copy, point, 5, color=(0, 0, 255), thickness=5)
-    #     imshow_(frame_copy)
 
     output_edge = 600 # square of 600 by 600
     output_points = np.array([[0, 0], [output_edge, 0], [output_edge, output_edge], [0, output_edge]], dtype=np.float32)
@@ -683,44 +475,14 @@ def master(frame):
     white_stones_transf = white_stones_transf[(white_stones_transf[:, 0:2] >= 0).all(axis=1) & (white_stones_transf[:, 0:2] <= output_edge).all(axis=1)]
     
         
-    # image, gray = get_blurred_img(transformed_image)
-    # print(1)
-
-    # canny = Canny_(gray)
-    # # return canny
-    # print(2)
-
-    # a = np.zeros_like(transformed_image)
-    # lines_, a = HoughLinesP_(canny, transformed_image)
-    # cv2.imwrite("results/houghline.jpg", a)
-    # print(3)
-    
-    # if lines_ is None:
-    #     raise Exception(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>no lines ware detected")
-    
-    # if len(lines_) == 0:
-    #     raise Exception(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>no lines ware detected")
-
-    # lines = clean_lines(lines_.reshape((-1, 4)), image_width=transformed_image.shape[1], image_height=transformed_image.shape[0])
-
-    # print(4)
-
-    # cluster_1, cluster_2 = cluster_orientation(lines)
-    # print(5)
+   
     cluster_1 = vertical_lines[(vertical_lines<=600).all(axis=1) & (vertical_lines>=0).all(axis=1)]
     cluster_2 = horizontal_lines[(horizontal_lines<=600).all(axis=1) & (horizontal_lines>=0).all(axis=1)]
     
-    draw_lines(cluster_1.astype(dtype=int), transformed_image, color=(0, 0, 255))
-    draw_lines(cluster_2.astype(dtype=int), transformed_image, color=(0, 255, 0))
+
     
     cv2.imwrite("results/transformed_image.jpg", transformed_image)
-    # #######
-    # if len(cluster_1) > 1:
-    #     cluster_1 = restore_missing_lines(cluster_1)
-
-    # if len(cluster_2) > 1:
-    #     cluster_2 = restore_missing_lines(cluster_2)
-    # #######
+ 
 
     intersections = detect_intersections(cluster_1, cluster_2, transformed_image)
     
@@ -730,7 +492,6 @@ def master(frame):
     print(6)
     moves = define_moves(white_stones_transf, black_stones_transf, intersections)
 
-    # # draw_points(np.append(white_stones_transf, black_stones_transf, axis=0).astype(dtype=int), image)
     
     sgf_ = GoSgf('a', 'b', moves)
     sgf_f, sgf_n = sgf_.createSgf()
@@ -741,16 +502,10 @@ def master(frame):
     # return transformed_image
     return board.final_position()
 
-# %%
-import cv2
-import threading
-import queue
-from ultralytics import YOLO
-import copy
-import traceback
+
 
 # %%
-def process_frames(queue):
+def process_frames():
     global ProcessFrame
     while True:
         # if queue.not_empty:
@@ -782,10 +537,9 @@ def process_frames(queue):
 
 model = YOLO('best8B.pt')
 
-frame_queue = queue.Queue()
 ProcessFrame = None
 
-process_thread = threading.Thread(target=process_frames, args=(frame_queue,))
+process_thread = threading.Thread(target=process_frames, args=())
 process_thread.start()
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # 0 for the default camera, change it if needed
@@ -911,62 +665,3 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 
-# # %%
-
-
-
-# def record():
-#     global frame, cap
-    
-    
-#       # 0 for the default camera, change it if needed
-#     while cap.isOpened():
-#         ret, frame = cap.read()
-#         if not ret:
-#             break
-        
-#         print("Resolution", frame.shape)
-
-#         cv2.imshow('Video Stream', frame)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-
-#             break 
-
-#     cap.release()
-#     cv2.destroyAllWindows()
-    
-
-# %%
-
-# model = YOLO('best8B.pt')
-
-# cap = cv2.VideoCapture(0)
-
-# frame = None
-
-
-# process_thread = threading.Thread(target=record, args=())
-# process_thread.start()
-
-
-
-# while True:
-#     # if queue.not_empty:
-#     if not frame is None:
-#         # frame = queue.get()  # Get a frame from the queue
-#         try:
-#             cv2.imshow("master", master(frame))
-#                 # Load the saved image using OpenCV
-#             # image = cv2.imread("result_image.jpg")
-
-#             # # Display the image using OpenCV
-#             # cv2.imshow("Result Image", image) # Process the frame
-#         except:
-#             print('empty frame')
-#         # cv2.imshow('Processed Stream', cv2.flip(frame, 1))  # Display the processed frame
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break  # Break the loop if 'q' is pressed
-    
-
-# cv2.destroyAllWindows()
-# %%
